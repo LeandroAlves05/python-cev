d5f8dcdc4af2b731bdb55d05cdecf88e91f64ccc
Leandro Alves <125614595+LeandroAlves05@users.noreply.github.com>
2023-05-03 11:57:54 -0400

    python #07

--- desafios/d004.py
+++ desafios/d004.py
@@ -5,4 +5,5 @@ print (f'São apenas espaços? {n.isspace()}')
 print (f'Está em caixa alta? {n.isupper()}')
 print (f'É um número decimal? {n.isdecimal()}')
 print (f'É alfanumérico? {n.isalnum()}')
+print (f'Está capitalizada? {n.istitle()}')
 
---
+++ scripts-python/aula07a.py
@@ -0,0 +1,9 @@
+n1 = int(input('Um valor: '))
+n2 = int(input('Outro valor: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print(f'A soma vale: {s}\nO produto é: {m}\nA divisão resulta em: {d:.3f}')
+print(f'Divisão inteira é: {di}\nE a potência é: {e}')

0e4a51a9db5da9cf25e4b60307764872298cf603
Leandro Alves <125614595+LeandroAlves05@users.noreply.github.com>
2023-05-04 15:59:33 -0400

    Update d010.py

--- desafios/d010.py
+++ desafios/d010.py
@@ -1,3 +1,3 @@
-din = float(input('Quanto dinheiro em reais você possui? : '))
-dol = din / 5.01
-print(f'Seu dinheiro em dólares equivale a ${dol:.3}')
+reais = float(input('Quanto dinheiro em reais você possui? : R$'))
+dol = reais / 5.01
+print(f'Seu dinheiro em dólares equivale a US${dol:.2f}')

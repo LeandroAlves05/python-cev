088d8b70c87d70f30b69edf11cd21df113c472fe
Leandro Alves <125614595+LeandroAlves05@users.noreply.github.com>
2023-06-07 17:39:13 -0400

    desafio

---
+++ desafios/d046.py
@@ -0,0 +1,8 @@
+from time import sleep
+print('PREPAREM SE PARA OS FOGOS!!')
+sleep(1.5)
+for c in range (10, -1, -1):
+    print(c)
+    sleep(1)
+print('E LA VAO OS FOGOS!!')
+print('AEEEEEEEEEE')
